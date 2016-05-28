12143a0d46b760490ecc5471115b888b6e0e7f51
Steve Mostovoy <stevemostovoysm@gmail.com>
2016-05-28 02:41:36 -0700

    Woops, that was a 20000 second sleep.

--- run.sh
+++ run.sh
@@ -58,7 +58,7 @@ if [ "$WERCKER_DEPLOY_TO_RANCHER_INPLACE" == true ]; then
   "$WERCKER_STEP_ROOT/rancher-compose" --url "$DTR_PROTO://$WERCKER_DEPLOY_TO_RANCHER_RANCHER_URL" --access-key "$WERCKER_DEPLOY_TO_RANCHER_ACCESS_KEY" --secret-key "$WERCKER_DEPLOY_TO_RANCHER_SECRET_KEY" --project-name "$WERCKER_DEPLOY_TO_RANCHER_STACK_NAME" up -d --upgrade "$WERCKER_DEPLOY_TO_RANCHER_SERVICE_NAME" --pull --interval 30000 --batch-size 1
   echo "Done."
   echo "Waiting 20 seconds to confirm upgrade..."
-  sleep 20000
+  sleep 20
   echo "Confirming upgrade..."
   "$WERCKER_STEP_ROOT/rancher-compose" --url "$DTR_PROTO://$WERCKER_DEPLOY_TO_RANCHER_RANCHER_URL" --access-key "$WERCKER_DEPLOY_TO_RANCHER_ACCESS_KEY" --secret-key "$WERCKER_DEPLOY_TO_RANCHER_SECRET_KEY" --project-name "$WERCKER_DEPLOY_TO_RANCHER_STACK_NAME" up -d --upgrade "$WERCKER_DEPLOY_TO_RANCHER_SERVICE_NAME" --confirm-upgrade
   echo "Done."
